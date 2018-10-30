8e6387af8443ab2781735b349fd0640bf6ac2ea0
Andy Lindsay <andydlindsay@gmail.com>
2018-10-30 19:52:23 +0000

    added function to generate random strings of characters

--- express_server.js
+++ express_server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString(stringLength) {
+  let outputString = '';
+  for(let i = 0; i < stringLength; i++) {
+    const randomNumber = Math.floor(Math.random() * 62) + 1;
+    if (randomNumber <= 10) {
+      outputString += String.fromCharCode(58 - randomNumber);
+    } else if (randomNumber <= 36) {
+      outputString += String.fromCharCode(101 - randomNumber);
+    } else {
+      outputString += String.fromCharCode(159 - randomNumber);
+    }
+  }
+  return outputString;
+}
 
 app.get('/urls.json', (request, response) => {
   response.json(urlDatabase);
@@ -18,16 +35,21 @@ app.get('/urls', (request, response) => {
   response.render('urls_index', templateVars);
 });
 
+app.post('/urls', (request, response) => {
+  console.log(request.body.longURL);
+  response.redirect('/urls');
+});
+
+app.get('/urls/new', (request, response) => {
+  response.render('urls_new');
+});
+
 app.get('/urls/:id', (request, response) => {
   let templateVars = {
     shortURL: request.params.id,
     longURL: urlDatabase[request.params.id]
   };
   response.render('urls_show', templateVars);
-});
-
-app.get('/urls_new', (request, response) => {
-  response.render('urls_new');
 });
 
 app.get('/hello', (request, response) => {
